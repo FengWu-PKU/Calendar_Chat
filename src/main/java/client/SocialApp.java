8474d93f429a5c92079c4ec47f34dc1c6d1aa350
AutumnKite <autumnkite233@gmail.com>
2023-05-29 21:21:19 +0800

    add client

---
+++ src/main/java/client/SocialApp.java
@@ -0,0 +1,56 @@
+package client;
+
+import java.awt.Font;
+import java.net.Socket;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.swing.UIManager;
+
+public class SocialApp {
+  private static Socket client;
+  private static ObjectOutputStream output;
+  private static ObjectInputStream input;
+
+  public static void connect(String serverAddress, int serverPort) throws IOException {
+    client = new Socket(serverAddress, serverPort);
+    output = new ObjectOutputStream(client.getOutputStream());
+    input = new ObjectInputStream(client.getInputStream());
+  }
+
+  public static Object readObject() {
+    try {
+      return input.readObject();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public static void writeObject(Object o) {
+    try {
+      output.writeObject(o);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void setDefaultFont() {
+    UIManager.put("Button.font", new Font("微软雅黑", 0, 14));
+    UIManager.put("Label.font", new Font("微软雅黑", 0, 14));
+    UIManager.put("OptionPane.font", new Font("微软雅黑", 0, 14));
+    UIManager.put("OptionPane.buttonFont", new Font("微软雅黑", 0, 14));
+    UIManager.put("OptionPane.messageFont", new Font("微软雅黑", 0, 14));
+    UIManager.put("Panel.font", new Font("微软雅黑", 0, 14));
+    UIManager.put("TextArea.font", new Font("微软雅黑", 0, 14));
+    UIManager.put("TextField.font", new Font("微软雅黑", 0, 14));
+    UIManager.put("PasswordField.font", new Font("微软雅黑", 0, 14));
+  }
+
+  public static void main(String[] args) {
+    setDefaultFont();
+    // new client.view.ServerConnectionFrame();
+    new client.view.LoginFrame();
+  }
+}
---
+++ src/main/java/client/view/LoginFrame.java
@@ -0,0 +1,97 @@
+package client.view;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ * 登录界面
+ */
+public class LoginFrame extends JFrame implements ActionListener {
+  private JTextField userNameField;
+  private JPasswordField passwordField;
+  private JButton loginButton;
+  private JButton registerButton;
+
+  public LoginFrame() {
+    // 窗口设置
+    setTitle("登录");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(300, 180);
+    setResizable(false);
+    setLocationRelativeTo(null);
+  
+    // 窗口布局
+    // 使用 EmptyBorder + GridBagLayout
+    JPanel contentPane = new JPanel();
+    contentPane.setBorder(new EmptyBorder(20, 20, 20, 20));
+    setContentPane(contentPane);
+    GridBagLayout layout = new GridBagLayout();
+    contentPane.setLayout(layout);
+
+    GridBagConstraints constraints = new GridBagConstraints();
+    constraints.fill = GridBagConstraints.HORIZONTAL;
+    constraints.insets = new Insets(5, 5, 5, 5);
+
+    JLabel userNameLabel = new JLabel("用户名:", SwingConstants.RIGHT);
+    userNameField = new JTextField();
+    JLabel passwordLabel = new JLabel("密码:", SwingConstants.RIGHT);
+    passwordField = new JPasswordField();
+
+    constraints.gridx = 0;
+    constraints.gridy = 0;
+    constraints.gridwidth = 1;
+    constraints.weightx = 0;
+    layout.setConstraints(userNameLabel, constraints);
+
+    constraints.gridx = 1;
+    constraints.gridy = 0;
+    constraints.gridwidth = 2;
+    constraints.weightx = 1;
+    layout.setConstraints(userNameField, constraints);
+
+    constraints.gridx = 0;
+    constraints.gridy = 1;
+    constraints.gridwidth = 1;
+    constraints.weightx = 0;
+    layout.setConstraints(passwordLabel, constraints);
+
+    constraints.gridx = 1;
+    constraints.gridy = 1;
+    constraints.gridwidth = 2;
+    constraints.weightx = 1;
+    layout.setConstraints(passwordField, constraints);
+
+    JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 5));
+    loginButton = new JButton("登录");
+    registerButton = new JButton("注册");
+    buttonPanel.add(loginButton);
+    buttonPanel.add(registerButton);
+
+    constraints.gridx = 0;
+    constraints.gridy = 2;
+    constraints.gridwidth = 3;
+    layout.setConstraints(buttonPanel, constraints);
+
+    contentPane.add(userNameLabel);
+    contentPane.add(userNameField);
+    contentPane.add(passwordLabel);
+    contentPane.add(passwordField);
+    contentPane.add(buttonPanel, constraints);
+
+    // 设置监听器
+    loginButton.addActionListener(this);
+
+    // 显示界面
+    setVisible(true);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == loginButton) {
+      
+    }
+  }
+}
