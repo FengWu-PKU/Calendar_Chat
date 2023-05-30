71c526022dddbf5e39b6190861ca448b0a0f8ffa
AutumnKite <autumnkite233@gmail.com>
2023-05-30 22:43:40 +0800

    common: add FriendItem

---
+++ src/main/java/common/FriendItem.java
@@ -0,0 +1,70 @@
+package common;
+
+import java.time.LocalDateTime;
+
+/**
+ * 好友列表中单个好友条目所需的信息
+ */
+public class FriendItem implements java.io.Serializable, Comparable<FriendItem> {
+  private static final long serialVersionUID = 1L;
+
+  private String username;
+  private String remark;
+  private String lastMessage;
+  private LocalDateTime lastMessageTime;
+  private int unreadMessages;
+
+  public FriendItem(String username, String remark, String lastMessage, LocalDateTime lastMessageTime,
+      int unreadMessages) {
+    this.username = username;
+    this.remark = remark;
+    this.lastMessage = lastMessage;
+    this.lastMessageTime = lastMessageTime;
+    this.unreadMessages = unreadMessages;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getRemark() {
+    return remark;
+  }
+
+  public void setRemark(String remark) {
+    this.remark = remark;
+  }
+
+  public String getLastMessage() {
+    return lastMessage;
+  }
+
+  public void setLastMessage(String lastMessage) {
+    this.lastMessage = lastMessage;
+  }
+
+  public LocalDateTime getLastMessageTime() {
+    return lastMessageTime;
+  }
+
+  public void setLastMessageTime(LocalDateTime lastMessageTime) {
+    this.lastMessageTime = lastMessageTime;
+  }
+
+  public int getUnreadMessages() {
+    return unreadMessages;
+  }
+
+  public void setUnreadMessages(int unreadMessages) {
+    this.unreadMessages = unreadMessages;
+  }
+
+  @Override
+  public int compareTo(FriendItem o) {
+    return -lastMessageTime.compareTo(o.lastMessageTime);
+  }
+}
