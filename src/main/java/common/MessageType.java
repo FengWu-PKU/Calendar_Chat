1bd2750d7ace82951899c5bb7132c37b48c3afe8
AutumnKite <autumnkite233@gmail.com>
2023-05-30 18:46:51 +0800

    common: add REGISTER_XXX

--- src/main/java/common/MessageType.java
+++ src/main/java/common/MessageType.java
@@ -4,6 +4,9 @@ package common;
  * 发送给服务器的消息类型
  */
 public enum MessageType {
+  
+  // 以下为服务器端到客户端的消息
+
   /**
    * 登录成功
    */
@@ -20,14 +23,26 @@ public enum MessageType {
   ALREADY_LOGIN,
 
   /**
-   * 获取好友列表
+   * 注册成功
    */
-  GET_FRIENDS,
+  REGISTER_SUCCEED,
+
+  /**
+   * 注册失败
+   */
+  REGISTER_FAILED,
 
   /**
    * 返回好友列表
    */
   RET_FRIENDS,
+
+  // 以下为客户端到服务端的消息
+
+  /**
+   * 获取好友列表
+   */
+  GET_FRIENDS,
 
   // TODO: more operations...
 }
