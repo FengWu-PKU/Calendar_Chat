570704a782c17330623697a5c69e04930136383d
Feng Wu <2000013727@stu.pku.edu.cn>
2023-05-30 10:30:00 +0800

    complete ToDoList

--- src/main/java/server/utils/ToDoList.java
+++ src/main/java/server/utils/ToDoList.java
@@ -7,12 +7,15 @@ public class ToDoList {
     static String username = "public_user";
     static String password = "123456";
 
+    static final int MAXTODOLISTNUM=10;
+
     int account_id;
     String usr_name;
     String entry_name;
     String content;
     Timestamp ddl;
     boolean alarm;
+    boolean complete;
 
     public ToDoList(int account_id, String user_name, String entry_name, String content, Timestamp ddl, boolean alarm) {
         this.account_id = account_id;
@@ -21,6 +24,17 @@ public class ToDoList {
         this.content = content;
         this.ddl = ddl;
         this.alarm = alarm;
+        this.complete=false;
+    }
+
+    public ToDoList(int account_id, String user_name, String entry_name, String content, Timestamp ddl, boolean alarm, boolean complete) {
+        this.account_id = account_id;
+        this.usr_name = user_name;
+        this.entry_name = entry_name;
+        this.content = content;
+        this.ddl = ddl;
+        this.alarm = alarm;
+        this.complete=complete;
     }
 
     /*插入新的条目*/
@@ -40,4 +54,136 @@ public class ToDoList {
             throw new RuntimeException(e);
         }
     }
+
+    /*删除条目*/
+    static void deleteEntry(int account_id, String entry_name) {
+        try (Connection connection=DriverManager.getConnection(url, username, password)) {
+            String sql="DELETE FROM todolist WHERE account_id=? AND entry_name=?";
+            PreparedStatement stmt=connection.prepareStatement(sql);
+            stmt.setInt(1, account_id);
+            stmt.setString(2, entry_name);
+            stmt.executeUpdate();
+            stmt.close();
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**更改条目, 前两个参数是旧的条目信息，第三个参数是新的条目*/
+    static void modifyEntry(int account_id, String entry_name, ToDoList toDoList) {
+        assert(toDoList.account_id==account_id);
+        try (Connection connection=DriverManager.getConnection(url, username, password)) {
+            //先删除旧的，在插入新的
+            String sql="DELETE FROM todolist WHERE account_id=? AND entry_name=?";
+            PreparedStatement stmt=connection.prepareStatement(sql);
+            stmt.setInt(1, account_id);
+            stmt.setString(2, entry_name);
+            stmt.executeUpdate();
+            stmt.close();
+            ToDoList.insertEntry(toDoList);
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*将某个条目设置为完成*/
+    static void completeEntry(int account_id, String entry_name) {
+        try (Connection connection=DriverManager.getConnection(url, username, password)) {
+            String sql="UPDATE todolist SET complete=TRUE WHERE account_id=? AND entry_name=?";
+            PreparedStatement stmt=connection.prepareStatement(sql);
+            stmt.setInt(1, account_id);
+            stmt.setString(2, entry_name);
+            stmt.executeUpdate();
+            stmt.close();
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*查询所有已经完成的条目*/
+    static ToDoList[] findCompletedEntry(int account_id) {
+        return findCompletedORNotEntry(account_id, true);
+    }
+
+    /*查询所有未完成的条目*/
+    static ToDoList[] findNotCompletedEntry(int account_id) {
+        return findCompletedORNotEntry(account_id, false);
+    }
+
+    static ToDoList[] findCompletedORNotEntry(int account_id, boolean comp) {
+        ToDoList[] ans=new ToDoList[MAXTODOLISTNUM];
+        try(Connection connection=DriverManager.getConnection(url, username, password)) {
+            String sql="SELECT * FROM todolist WHERE account_id=? AND complete=?";
+            PreparedStatement stmt=connection.prepareStatement(sql);
+            stmt.setInt(1, account_id);
+            stmt.setBoolean(2, comp);
+            ResultSet res= stmt.executeQuery();
+            int cnt=0;
+            while(res.next()) {
+                String usr_name=res.getString("usr_name");
+                String entry_name=res.getString("entry_name");
+                String content=res.getString("content");
+                Timestamp ddl=res.getTimestamp("ddl");
+                boolean alarm=res.getBoolean("alarm");
+                ToDoList cur=new ToDoList(account_id, usr_name, entry_name, content, ddl, alarm, comp);
+                ans[cnt++]=cur;
+                if(cnt>=MAXTODOLISTNUM) break;
+            }
+            return ans;
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*根据条目名称查询，条目名不重复*/
+    static ToDoList findNameEntry(int account_id, String name) {
+        try(Connection connection=DriverManager.getConnection(url, username, password)) {
+            String sql="SELECT * FROM todolist WHERE account_id=? AND entry_name=?";
+            PreparedStatement stmt=connection.prepareStatement(sql);
+            stmt.setInt(1, account_id);
+            stmt.setString(2, name);
+            ResultSet res= stmt.executeQuery();
+            if(!res.next()) return null;  // 没有符合条件的条目
+
+            String usr_name=res.getString("usr_name");
+            String entry_name=res.getString("entry_name");
+            String content=res.getString("content");
+            Timestamp ddl=res.getTimestamp("ddl");
+            boolean alarm=res.getBoolean("alarm");
+            boolean complete=res.getBoolean("complete");
+            return new ToDoList(account_id, usr_name, entry_name, content, ddl, alarm, complete);
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*根据内容查找*/
+    static ToDoList[] findContentEntry(int account_id, String cont) {
+        ToDoList[] ans=new ToDoList[MAXTODOLISTNUM];
+        try(Connection connection=DriverManager.getConnection(url, username, password)) {
+            String sql="SELECT * FROM todolist WHERE account_id=?";
+            PreparedStatement stmt=connection.prepareStatement(sql);
+            stmt.setInt(1, account_id);
+            ResultSet res= stmt.executeQuery();
+            int cnt=0;
+            while(res.next()) {
+                String usr_name=res.getString("usr_name");
+                String entry_name=res.getString("entry_name");
+                String content=res.getString("content");
+                if(!content.contains(cont)) continue; // 只有包含查询串的条目被返回
+                Timestamp ddl=res.getTimestamp("ddl");
+                boolean alarm=res.getBoolean("alarm");
+                boolean complete=res.getBoolean("complete");
+                ToDoList cur=new ToDoList(account_id, usr_name, entry_name, content, ddl, alarm, complete);
+                ans[cnt++]=cur;
+                if(cnt>=MAXTODOLISTNUM) break;
+            }
+            return ans;
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 }
--- src/test/java/server/utils/ToDoListTest.java
+++ src/test/java/server/utils/ToDoListTest.java
@@ -13,4 +13,30 @@ class ToDoListTest {
         ToDoList toDoList=new ToDoList(1, "Alice", "study", "写完数据库操作", new Timestamp(System.currentTimeMillis()), true);
         ToDoList.insertEntry(toDoList);
     }
+
+    @Test
+    void deleteEntry() {
+        ToDoList.deleteEntry(1, "study");
+    }
+
+    @Test
+    void modifyEntry() {
+        ToDoList tmp=new ToDoList(1, "Alice", "labs", "更新的内容", new Timestamp(System.currentTimeMillis()), true);
+        ToDoList.modifyEntry(1, "study", tmp);
+    }
+
+    @Test
+    void findNotCompletedEntry() {
+        ToDoList[] ans=ToDoList.findNotCompletedEntry(1);
+        for(int i=0;i<ToDoList.MAXTODOLISTNUM&&ans[i]!=null; i++) {
+            System.out.print(ans[i].usr_name);
+            System.out.print(", ");
+            System.out.print(ans[i].entry_name);
+            System.out.print(", ");
+            System.out.print(ans[i].content);
+            System.out.print(", ");
+            System.out.print(ans[i].ddl);
+        }
+    }
+
 }
